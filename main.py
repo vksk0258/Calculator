f8fd41783fd5f5d7aafa8b8646887e47c7930c5b
giltaek.yu <vksk0258@gmail.com>
2023-03-26 10:16:18 +0900

    텍스트 에디트 추가 및 버튼 함수 수정

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
-                             QMessageBox)
+                             QMessageBox,QPlainTextEdit)
 from PyQt5.QtGui import QIcon
 
 
@@ -12,10 +12,14 @@ class Calculator(QWidget):
         self.init_UI()
         
     def init_UI(self):
+        self.te1 = QPlainTextEdit() # 텍스트 에디트 위젯 생성
+        self.te1.setReadOnly(True) # 텍스트 에디트 위젯을 읽기만 가능하도록 수정
+        
         self.btn1=QPushButton('Messsage',self) # 버튼 추가
         self.btn1.clicked.connect(self.activate_message) # 버튼 클릭 시 핸들러 함수 연결
         
         vbox=QVBoxLayout() # 수직 레이아웃 위젯 생성
+        vbox.addWidget(self.te1) # 수직 레이아웃에 텍스트 에디트 위젯 추가
         vbox.addStretch(1) # 빈 공간
         vbox.addWidget(self.btn1) # 버튼 위치
         vbox.addStretch(1)
@@ -28,8 +32,9 @@ class Calculator(QWidget):
         self.resize(256,256)
         self.show()
         
-    def activate_message(self): # 버튼을 클릭할 때 동작하는 함수 : 메시지 박스 출력
-        QMessageBox.information(self,"information", "Button clicked!")
+    def activate_message(self): # 핸들러 함수 수정 : 메시지가 텍스트 에디트에 출력되도록
+        #QMessageBox.information(self,"information", "Button clicked!")
+        self.te1.appendPlainText("Butteon clicked!")
         
 if __name__=='__main__':
     app = QApplication(sys.argv)
