6c09c1d37fc3712888c3fc164e2539062c5f2656
giltaek.yu <vksk0258@gmail.com>
2023-03-28 15:44:53 +0900

    클리어 버튼 추가 및 핸들러 함수 추가

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
-                             QMessageBox,QPlainTextEdit)
+                             QMessageBox,QPlainTextEdit, QHBoxLayout)
 from PyQt5.QtGui import QIcon
 
 
@@ -18,10 +18,18 @@ class Calculator(QWidget):
         self.btn1=QPushButton('Messsage',self) # 버튼 추가
         self.btn1.clicked.connect(self.activate_message) # 버튼 클릭 시 핸들러 함수 연결
         
+        self.btn2=QPushButton('Clear',self) # 버튼2 추가
+        self.btn2.clicked.connect(self.clear_message) # 버튼2 클릭 시 핸들러 함수 연결
+        
+        hbox = QHBoxLayout() #수평 박스 레이아웃을추가하고 버튼1, 2 추가
+        hbox.addStretch(1) # 공백
+        hbox.addWidget(self.btn1) # 버튼 1 배치
+        hbox.addWidget(self.btn2) # 버튼 2 배치 
+        
         vbox=QVBoxLayout() # 수직 레이아웃 위젯 생성
         vbox.addWidget(self.te1) # 수직 레이아웃에 텍스트 에디트 위젯 추가
-        vbox.addStretch(1) # 빈 공간
-        vbox.addWidget(self.btn1) # 버튼 위치
+        #vbox.addWidget(self.btn1) # 버튼 위치
+        vbox.addLayout(hbox) # btn1 위치에 hbox를 배치
         vbox.addStretch(1)
         
         self.setLayout(vbox) # 빈 공간 - 버튼 - 빈 공간 순으로 수직 배치된 레이아웃 설정
@@ -35,6 +43,9 @@ class Calculator(QWidget):
     def activate_message(self): # 핸들러 함수 수정 : 메시지가 텍스트 에디트에 출력되도록
         #QMessageBox.information(self,"information", "Button clicked!")
         self.te1.appendPlainText("Butteon clicked!")
+        
+    def clear_message(self): # 버튼 2 핸들러 함수 정의
+        self.te1.clear()
         
 if __name__=='__main__':
     app = QApplication(sys.argv)
